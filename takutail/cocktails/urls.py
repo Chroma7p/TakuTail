524ae47eceb5b2f8c2fa6d16a9a42743d31fc47f
Chroma7p <nahco3.hcl@gmail.com>
2024-08-11 05:09:06 +0900

    テンプレートの一部をbaseに変更

--- takutail/cocktails/urls.py
+++ takutail/cocktails/urls.py
@@ -6,7 +6,7 @@ router = DefaultRouter()
 router.register(r'cocktails', views.CocktailViewSet)
 
 urlpatterns = [
-    path('', views.cocktail_list, name='cocktail_list'),
+    path('', views.cocktail_list, name='cocktails_list'),
     path('create_random_cocktail/', views.create_random_cocktail, name='create_random_cocktail'),
     path('<int:pk>/', views.cocktail_detail, name='cocktail_detail'),
     path('list_possible_cocktails/', views.list_cocktails, name='list_possible_cocktails'),
